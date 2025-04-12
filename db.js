914124e722bf1ee9c5bb9833237142f46ce564a7
Murali Mohan <murhar659@gmail.com>
2025-04-12 08:54:56 +0530

    Added render postgres

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'deals_db',
-  password: 'Postgresql@123',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
 });
 
 module.exports = pool
